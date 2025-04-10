512f312779525e61b60fc8a36d8facf101e76b73
Main <you@example.com>
2025-04-10 17:14:06 +1000

    За 10.04.2025

--- Assets/Game/Scripts/Enemy.cs
+++ Assets/Game/Scripts/Enemy.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private List<Transform> _wayPoints;
+    [SerializeField] private float _speed;
+    private List<Transform> _wayPoints = new List<Transform>();
 
     internal void SetWay(List<Transform> wayPoints)
     {
-        _wayPoints = wayPoints;
-    }
-
-    void Start()
-    {
-        
+        _wayPoints = new List<Transform>(wayPoints);
     }
 
     void Update()
     {
-        
+        if (_wayPoints.Count > 0 && Vector3.Distance(
+            transform.position, _wayPoints[0].position)
+            > 0.3f)
+        {
+            transform.LookAt(_wayPoints[0].position);
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                _wayPoints[0].position,
+                _speed * Time.deltaTime);
+        }
+        else if (_wayPoints.Count > 0 && Vector3.Distance(
+            transform.position, _wayPoints[0].position)
+            <= 0.3f)
+        {
+            _wayPoints.RemoveAt(0);
+            if (_wayPoints.Count == 0)
+                Destroy(gameObject);
+        }
+
     }
 }
