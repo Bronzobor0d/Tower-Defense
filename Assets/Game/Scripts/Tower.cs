9420114e44d7f13a5238c10ae60006fc10e5a5a8
Main <you@example.com>
2025-05-22 16:47:34 +1000

    За 22.05.25 1

--- Assets/Game/Scripts/Tower.cs
+++ Assets/Game/Scripts/Tower.cs
@@ -1,31 +1,59 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    [SerializeField] private GameObject _bullet;
+    [SerializeField] private Transform _bulletSpawn;
+    [SerializeField] private float _reloadTime;
+
+    public Enemy Target;
     public List<Enemy> Enemies = new List<Enemy>();
+
+    private bool _isShoot;
 
     internal void RemoveEnemy(Enemy enemy)
     {
         if (Enemies.Contains(enemy))
             Enemies.Remove(enemy);
+        if (enemy == Target)
+        {
+            Target = null;
+            SetNewTarget();
+        }
+    }
+
+    private void SetNewTarget()
+    {
+        if (Enemies.Count > 0)
+            Target = Enemies.Last();
     }
 
     internal void SetEnemy(Enemy enemy)
     {
         if (!Enemies.Contains(enemy))
             Enemies.Add(enemy);
-    }
-
-    void Start()
-    {
-        
+        if (Target == null)
+            SetNewTarget();
     }
 
     void Update()
     {
-        
+        if (Target != null && !_isShoot)
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+
+    private IEnumerator Shoot()
+    {
+        _isShoot = true;
+        yield return new WaitForSeconds(_reloadTime);
+        GameObject bulletObj = Instantiate(_bullet, _bulletSpawn.position,
+            Quaternion.identity);
+        _isShoot = false;
     }
 }
